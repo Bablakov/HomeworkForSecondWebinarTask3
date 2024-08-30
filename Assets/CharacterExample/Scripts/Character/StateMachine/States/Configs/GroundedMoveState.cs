c24a95252bcc4c884ddda2e6ea672787a588d3ac
Bablakov <127343444+Bablakov@users.noreply.github.com>
2024-08-30 14:04:06 +0300

    Стартовый комит

---
+++ Assets/CharacterExample/Scripts/Character/StateMachine/States/Configs/GroundedMoveState.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class GroundedMoveState
+{
+    [field: SerializeField, Range(0, 10)] public float WalkingSpeed { get; private set; }
+    [field: SerializeField, Range(0, 10)] public float RunningSpeed { get; private set; }
+    [field: SerializeField, Range(0, 10)] public float RunningFastSpeed { get; private set; }
+}
